68231b6c1a1b9eed33634cc040efa5070e9eb8f9
Joqtán Mármol <joqtanmarmol@gmail.com>
2025-08-28 16:00:26 -0600

    Mejora el formato del código en home_screen.dart
    Se ajusta el formato del código eliminando saltos de línea innecesarios y mejorando la legibilidad de las funciones de carga de películas.
    
    Esto facilita el mantenimiento y la comprensión del código a largo plazo.

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -54,11 +54,9 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          floating: true, 
+          floating: true,
           flexibleSpace: FlexibleSpaceBar(title: CustomAppbar()),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search),),
-          ],
+          actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
         ),
         SliverList(
           delegate: SliverChildBuilderDelegate((context, index) {
@@ -69,28 +67,23 @@ class _HomeViewState extends ConsumerState<_HomeView> {
                   movies: nowPlayingMovies,
                   title: 'En cines',
                   subTitle: 'Lunes 12',
-                  loadNextPage: () => 
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
+                  loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
                 ),
                 MovieHorizontalListview(
                   movies: popularMovies,
                   title: 'Populares',
-                  loadNextPage: () => 
-                    ref.read(popularMoviesProvider.notifier).loadNextPage(),
+                  loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
                 ),
                 MovieHorizontalListview(
                   movies: topRatedMovies,
                   title: 'Mejor calificadas',
-                  loadNextPage: () => 
-                    ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
+                  loadNextPage: () => ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
                 ),
                 MovieHorizontalListview(
                   movies: upcomingMovies,
                   title: 'Proximamente',
-                  loadNextPage: () => 
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
+                  loadNextPage: () => ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
                 ),
-                
               ],
             );
           }, childCount: 1),
