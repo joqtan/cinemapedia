4855adab30a69135da7b3d518f34c81da397c96c
Joqtán Mármol <joqtanmarmol@gmail.com>
2025-09-23 16:34:08 -0600

    Agrega la funcionalidad para cargar actores por película
    Se implementa un nuevo proveedor para obtener actores asociados a una película específica.
    
    Se añade la lógica para cargar actores en la pantalla de películas y se corrige la forma de asignar el departamento en el modelo de respuesta de créditos.
    
    Esto mejora la modularidad del código y facilita la gestión de actores en la aplicación.

---
+++ lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorsByMovieProvider = StateNotifierProvider<ActorsByMovieNotifier, Map<String, List<Actor>>>((ref) {
+  final getMovieById = ref.watch(actorsRepositoryProvider);
+  return ActorsByMovieNotifier(getActors: getMovieById.getActorsByMovie);
+});
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String id);
+
+class ActorsByMovieNotifier extends StateNotifier<Map<String, List<Actor>>> {
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifier({required this.getActors}) : super({});
+
+  Future<void> loadActors(String movieId) async {
+    if (state.containsKey(movieId)) return;
+    print('realizando peticion http');
+    final actors = await getActors(movieId);
+
+    state = {...state, movieId: actors};
+  }
+}
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -19,6 +19,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   void initState() {
     super.initState();
     ref.read(moviesDetailProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
